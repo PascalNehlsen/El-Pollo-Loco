8021fdc82cbfd01c2a85fc88c9c904f00fb9426d
Pascal Nehlsen <pascaldev92@gmail.com>
2024-07-02 12:46:56 +0200

    added walking sound

--- models/character.class.js
+++ models/character.class.js
@@ -13,6 +13,7 @@ class Character extends MovableObject {
   ]
   world;
   speed = 10;
+  walking_sound = new Audio('./audio/footsteps.mp3');
 
   constructor() {
     super().loadImage(
@@ -26,17 +27,21 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-      if (this.world.keyboard.RIGHT) {
+      this.walking_sound.pause();
+      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.x += this.speed;
         this.otherDirection = false;
+        this.walking_sound.play();
       }
     }, 1000 / 60)
 
     setInterval(() => {
-      if (this.world.keyboard.LEFT) {
+      if (this.world.keyboard.LEFT && this.x > -200) {
         this.x -= this.speed;
         this.otherDirection = true;
+        this.walking_sound.play();
       }
+      this.world.camera_x = -this.x + 100;
     }, 1000 / 60)
 
     setInterval(() => {
